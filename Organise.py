95819637811f7f66edae9d3085ad0030a668c747
Raj Maharjan <34892967+dubbyding@users.noreply.github.com>
2020-01-23 09:35:46 +0545

    done with comments

--- Organise.py
+++ Organise.py
@@ -1,25 +1,26 @@
 import os
 import fleep  
-import moving
-user=moving.username()
-path_ = 'C:\\Users\\'+user+'\\Downloads\\'
-path2_ = 'C:\\Users\\'+user+'\\Documents\\'
-for entry in os.scandir(path = path2_):
-    if entry.is_dir():
+import move
+user=move.username() #Check Username of logged in user
+#Path of Download folder and Documents folder
+path_ = 'C:/Users/' +user+'/Downloads/'
+path2_ = 'C:/Users/'+user+'/Documents/'
+for entry in os.scandir(path = path2_):  #Scans dir of path of Documents
+    if entry.is_dir():  #Checks if it is dir
         continue
-    ext = entry.name.split('.')[-1]
-    moving.moveByExt(path2_ , ext, entry.name)
-for entry in os.scandir(path=path_):
-    if entry.is_dir():
+    ext = entry.name.split('.')[-1] #gets extension for a file
+    move.moveByExt(path2_ , ext, entry.name)  #move file by extension
+for entry in os.scandir(path=path_):    #Scan dir of path of Downloads
+    if entry.is_dir():  #check if it is dir
         continue
-    ext = entry.name.split('.')[-1]
-    with open(path_+'/'+entry.name,"rb") as file:
-        DataType = fleep.get(file.read(128)).type
-    if not DataType:
-        moving.moveByExt(path_ , ext, entry.name)
-    move_ = moving.checkFileType(DataType,user,path_)
-    if move_ == 'None':
-        moving.moveByExt(path_ , ext, entry.name)
+    ext = entry.name.split('.')[-1] #gets extension for a file
+    with open(path_+'/'+entry.name,"rb") as file: #Opens that file as a binary file
+        DataType = fleep.get(file.read(128)).type   #fleep gets the file type
+    if not DataType:    #Somefile's data type is not returned hence those are sorted in download folder by extension
+        move.moveByExt(path_ , ext, entry.name)
+        continue
+    move_ = move.checkFileType(DataType,user,path_) #check file type
+    if move_ == 'None': 
+        move.moveByExt(path_ , ext, entry.name)
     else:
-        moving.moveToRespectiveFolder(move_,entry.name,path_)
-    
+        move.moveToRespectiveFolder(move_,entry.name,path_)
